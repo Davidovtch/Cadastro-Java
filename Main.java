eec47e24895f333d1ed1ea6c4c124a0428981746
E <eriq2077@gmail.com>
2023-08-09 13:54:56 -0300

    Verifies if the system if full or empty before adding or removing someone

--- Main.java
+++ Main.java
@@ -2,37 +2,47 @@ package OOP;
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-       Scanner sc=new Scanner(System.in);
-       int numberOfPeople=0;
-       Person[] people=new Person[5];
-       int keepRunning=1;
-       while (keepRunning==1) {
+        Scanner sc=new Scanner(System.in);
+        int numberOfPeople=0;
+        Person[] people=new Person[5];
+        int keepRunning=1;
+        while (keepRunning==1) {
         System.out.println("What you want to do?\n1-Register a person\n2-Remove a person\n3-Show registred people\n4-Leave");
         int choice=sc.nextInt();
         System.out.println("======================");
         switch (choice) {
-            case 1:       
-                people[numberOfPeople]=new Person("", 0, "", "");
-                System.out.println("Type name:");
-                String name=sc.next();
-                //System.out.println("Type age:");
-                //int age=sc.nextInt();
-                //System.out.println("Type job:");
-                //String job=sc.next();
-                //System.out.println("Type sex:");
-                //String sex=sc.next();
-                people[numberOfPeople].nameset(name);
-                //people[numberOfPeople].ageset(age);
-                //people[numberOfPeople].jobset(job);
-                //people[numberOfPeople].sexset(sex);
-                numberOfPeople++;
+            case 1:
+                if(numberOfPeople==5){
+                    System.out.println("The system is full!\nYou can no longer register people...");
+                }
+                else{
+                    people[numberOfPeople]=new Person("", 0, "", "");
+                    System.out.println("Type name:");
+                    String name=sc.next();
+                    //System.out.println("Type age:");
+                    //int age=sc.nextInt();
+                    //System.out.println("Type job:");
+                    //String job=sc.next();
+                    //System.out.println("Type sex:");
+                    //String sex=sc.next();
+                    people[numberOfPeople].nameset(name);
+                    //people[numberOfPeople].ageset(age);
+                    //people[numberOfPeople].jobset(job);
+                    //people[numberOfPeople].sexset(sex);
+                    numberOfPeople++;
+                }
                 System.out.println("======================");
                 break;
             case 2:
-                System.out.println("Type the number of the person you want to remove:");
-                int remove=sc.nextInt();
-                people[remove-1]=null;
-                System.out.println("Person removed!");
+                if (numberOfPeople==0) {
+                    System.out.println("The system is empty!\nThere is no one to remove...");
+                }
+                else{
+                    System.out.println("Type the number of the person you want to remove:");
+                    int remove=sc.nextInt();
+                    people[remove-1]=null;
+                    System.out.println("Person removed!");
+                }
                 System.out.println("======================");
                 break;
             case 3:
@@ -48,8 +58,8 @@ public class Main {
                 keepRunning=0;
                 break;
             default:
-            System.out.println("Invalid...\nTry again!");
-            System.out.println("======================");
+                System.out.println("Invalid...\nTry again!");
+                System.out.println("======================");
                 break;
        }
     }
